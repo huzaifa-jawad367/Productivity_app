85a9b82a2e81f48699f34b822ef9483401d95d4b
huzaifa-jawad367 <hjawad.bscs21seecs@seecs.edu.pk>
2024-09-20 12:00:24 +0500

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Huzaifa Jawad on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
